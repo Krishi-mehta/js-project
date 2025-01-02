719ade390fde40cf4b2079e1b79ca363c42adcd1
Krishi-mehta <117520846+Krishi-mehta@users.noreply.github.com>
2025-01-02 16:27:32 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 123
+let accountEmail = "123@gmail.com"
+var accPass = "12345"
+accCity = "mumbai"
+
+//accountId = 2  // gives and err
+
+console.log(accountId)
+
+accountEmail = "456@gamil.com"
+accPass = "678"
+accCity =  "baroad"
+
+console.log([accountEmail, accPass, accCity ])
