4599d641b7198b7940391fc650db7c8e08944f9a
Krishi-mehta <117520846+Krishi-mehta@users.noreply.github.com>
2025-01-04 09:11:30 +0000

    container config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+//dataypes
+
+let number = 1
+let string = "krishi"
+let boolean = true
+let name = null
+let city;
+
+console.log(number)
+console.log(string)
+console.log(boolean)
+console.log(city)
+
+//null is object it is not empty value it is a value that we perfer as a null
+//underfined means it value is not defined
+
+//datatypes conversion
+
+let booleanTrue =String(boolean)
+console.log(booleanTrue)
+console.log(typeof booleanTrue )
+
