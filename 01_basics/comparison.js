88cf7f9d838876dad34c0a06dbc5f7b7f2e8495d
Krishi-mehta <117520846+Krishi-mehta@users.noreply.github.com>
2025-01-05 12:05:35 +0000

    js

---
+++ 01_basics/comparison.js
@@ -0,0 +1,14 @@
+let num = 1
+let num1 = 1
+let numToBoolean = Boolean(num)
+
+console.log(num1 === numToBoolean)
+console.log(num1 == numToBoolean)
+
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,33 @@
+let str1 = "krishi"
+let str2 = "mehta"
+
+console.log(str1+str2);
+
+let num1 = "3"
+let num2 = 3
+
+console.log(num1 + num2);
+console.log("1"+1+1+1)
+console.log(1+1+1+"1")
+
+
+console.log(true);
+console.log(+true);
+console.log(+false);
+
+let number1 = 1
+let number2 = 2
+
+
+
+// console.log(number3);
+
+let numToString = String(number1)
+number1 = numToString
+
+let number3 = number1+number2
+console.log(typeof numToString);
+console.log(number3);
+
+
+
