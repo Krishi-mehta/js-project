7e879f655d3c8527a61b198eea0f9c8309091efd
Krishi-mehta <117520846+Krishi-mehta@users.noreply.github.com>
2025-01-16 10:34:10 +0000

    js

---
+++ 04_controlflow/ifelse.js
@@ -0,0 +1,43 @@
+// const userLogin = true
+const userLogin = false
+
+// if(userLogin){
+//     console.log("user just logged in");
+// }else{
+//     console.log("user not logged in");
+// }
+// if(userLogin){
+//     console.log("user just logged in");
+// }else{
+//     console.log("user not logged in");
+// }
+
+// const user = 16
+
+// if(user>=18){
+//     console.log(`${user} is valid age for driving`)
+// }
+// else{
+// console.log(`${user} is underage`)
+// }
+
+let a =2
+let b = 3
+let c =2
+
+// if(a>b && a>c){
+//     console.log("a is greater no")
+// }else if(b>a && b>c){
+//     console.log("b is greater no")
+// }else{
+//     console.log("c is greater no")
+// }
+
+if(a>b || b>c){
+    console.log("a is greater no")
+}else if(b>a || b>c){
+    console.log("b is greater no")
+}else{
+    console.log("c is greater no")
+}
+
---
+++ 05_iterations/for_loop.js
@@ -0,0 +1,45 @@
+// for(let i =0; i<=10; i++){
+//     console.log(`${i}`)
+// }
+
+// const array = [1,2,3,4,5]
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(element)
+// }
+
+
+// for(let i =1; i<=20; i++){
+//     if(i%2==0){
+//         console.log(`The number ${i} is even number`)
+//     }else{
+//         console.log(`The number ${i} is odd number`)
+//     }
+// }
+
+
+// for(let i= 1; i<=10; i++){
+//     if(i ==7){
+//         console.log(`${i} is my fav no`);
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// for(let i= 1; i<=10; i++){
+//     if(i ==7){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+
+
+for(let i=1; i<=10; i++){
+    // console.log(i)
+    for(let j=1; j<=10; j++){
+        // console.log(j)
+        console.log(i + "*" + j + " = " + i*j) 
+    }
+}
