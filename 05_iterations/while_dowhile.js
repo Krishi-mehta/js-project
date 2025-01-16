ca2f2147a7a2c32422579c90a6b32ffed8281013
Krishi-mehta <117520846+Krishi-mehta@users.noreply.github.com>
2025-01-16 11:07:52 +0000

    js

---
+++ 05_iterations/while_dowhile.js
@@ -0,0 +1,20 @@
+
+let i =0
+while (i<=10) {
+    console.log(i)
+    i++
+}
+
+let index=1  // if i write index=11 it will print bcoz this loop first do increment & then check condition
+do{
+console.log(index)
+index++
+}while(index<=10)
+
+let myArray = [1,2,3,4,5]
+arr = 0
+
+while(arr<myArray.length){
+    console.log(myArray[arr])
+    arr++
+}
